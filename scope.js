ea715206c0ff526af09b007e5009d58bfa4813ba
unknown <rahmanhabibur727@gemail.com>
2021-02-02 22:59:53 +0600

    ajker moto ses

---
+++ scope.js
@@ -0,0 +1,51 @@
+// system::-1
+// function sum (first, second){
+//     let result = first + second;
+//     return result;
+// }
+// const output = sum( 4, 7);
+// console.log(output);
+
+
+// system::-2
+// console.log(result);[return kora result k baire theke call kora jabena tobe variable declare kore call kora jabe]
+function sum (first, second){
+    let result = first + second;
+    console.log(result);
+    return result;
+}
+
+
+// system::-3
+// let bonus = 20;
+// function sum (first, second){
+//     let result = first + second + bonus;
+//     console.log(bonus);
+//     return result;
+// }
+// const output = sum( 4, 7);
+// console.log(bonus);
+// console.log(output);
+
+
+            // system:-4
+let bonus = 20;
+function sum (first, second){
+    let result = first + second + bonus;
+   
+    if( result>11){
+        var mood = 'happy';
+        var mood = 'fishy';
+        let mind = 'funky';
+       const mon = 'cranky';
+    console.log(mind);
+    console.log(mood);
+    console.log(mon);
+    // [Note:- const,let jukto variable k baire theke call kora jabena]
+  }
+  console.log(mood);
+    return result;
+}
+const output = sum( 4, 7);
+console.log(bonus);
+console.log(output);
